a6b84a08defa1204f6fd295e35c46c7e677d3624
Saeid Dadkhah <SaeidDadkhah>
2018-03-24 03:19:28 +0430

    removed some useless imports

--- statistical_feature_extraction/sfe.py
+++ statistical_feature_extraction/sfe.py
@@ -5,7 +5,6 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
